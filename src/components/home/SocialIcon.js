66da4abfa325675565212c352b48df99e15d2274
Cédric LEVY <levycedric@hotmail.fr>
2024-08-23 13:37:42 +0200

    comment

--- src/components/home/SocialIcon.js
+++ src/components/home/SocialIcon.js
@@ -5,8 +5,8 @@ function SocialIcon(props) {
     return (
         <a target="_blank" aria-label={label}
            rel="noopener noreferrer" href={link}> 
-           //!noopener est utilisé pour éviter une potentielle faille de sécurité liée à l'utilisation de liens qui s'ouvrent dans un nouvel onglet via target="_blank".
-           //!Masquer la provenance : Il empêche le navigateur d'envoyer des informations de référent à la page cible. Le référent est l'URL de la page d'où provient le clic. Si noreferrer est ajouté, la page vers laquelle pointe le lien ne saura pas d'où vient l'utilisateur (ce qui améliore la confidentialité).
+          {/* noopener est utilisé pour éviter une potentielle faille de sécurité liée à l'utilisation de liens qui s'ouvrent dans un nouvel onglet via target="_blank".
+        Masquer la provenance : Il empêche le navigateur d'envoyer des informations de référent à la page cible. Le référent est l'URL de la page d'où provient le clic. Si noreferrer est ajouté, la page vers laquelle pointe le lien ne saura pas d'où vient l'utilisateur (ce qui améliore la confidentialité).*/}
 
             <i className={icon} aria-hidden="true"/>
         </a>
